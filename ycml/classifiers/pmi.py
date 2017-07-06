8a3a07d18e7dfcdff76cc7e67c89494a6853f341
yc <yanchuan@outlook.com>
2017-07-06 14:17:14 +0800

    Implement standard PMI measure

--- ycml/classifiers/pmi.py
+++ ycml/classifiers/pmi.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class PMIFeatureSelector(MultiLabelsClassifier):
-    def __init__(self, algorithm='pmi', X_prior=0.001, Y_prior=0.1, XY_prior=1, **kwargs):
+    def __init__(self, algorithm='pmi', X_prior=0.001, Y_prior=0.1, XY_prior=0.001, **kwargs):
         super(MultiLabelsClassifier, self).__init__(**kwargs)
 
         self.algorithm = algorithm
@@ -16,34 +16,29 @@ class PMIFeatureSelector(MultiLabelsClassifier):
     #end def
 
     def fit_binarized(self, X_featurized, Y_binarized, **kwargs):
-        log_P_x = np.asarray(X_featurized.sum(axis=0) + self.X_prior).ravel()
-        log_P_x = np.log(log_P_x) - np.log(log_P_x.sum())
-
-        log_P_y = Y_binarized.sum(axis=0) + self.Y_prior
-        log_P_y = np.log(log_P_y) - np.log(log_P_y.sum())
-
-        log_P_xy = np.zeros((X_featurized.shape[1], Y_binarized.shape[1]))
+        log_Nx = np.log(np.asarray((X_featurized.sum(axis=0) + self.X_prior)).ravel())
+        log_Ny = np.log(Y_binarized.sum(axis=0) + self.Y_prior)
+        Y_indexes = [set(np.flatnonzero(Y_binarized[:, j] > 0).tolist()) for j in range(Y_binarized.shape[1])]
+        pmi_XY = np.zeros((X_featurized.shape[1], Y_binarized.shape[1]))
         for i in range(X_featurized.shape[1]):
+            X_indexes = set(np.flatnonzero(np.asarray((X_featurized[:, i] > 0).todense()).ravel().flatten()).tolist())
             for j in range(Y_binarized.shape[1]):
-                X_indexes = (X_featurized[:, i] > 0).todense().flatten()
-                Y_indexes = Y_binarized[:, j] > 0
-                log_P_xy[i, j] = np.logical_and(X_indexes, Y_indexes).sum() + self.XY_prior
+                log_Nxy = len(X_indexes & Y_indexes[j]) + self.XY_prior
+                pmi_XY[i, j] = log_Nxy - log_Nx[i] - log_Ny[j]
             #end for
         #end for
-        log_P_xy = np.log(log_P_xy) - np.log(log_P_xy.sum())
+        print(np.max(pmi_XY, axis=1))
 
-        pmi = log_P_xy
-        for i in range(X_featurized.shape[1]):
-            pmi[i, :] -= log_P_x[i]
-        for j in range(Y_binarized.shape[1]):
-            pmi[:, j] -= log_P_y[j]
-
-        self.feature_scores_ = np.max(pmi, axis=1)
+        self.pmi_ = pmi_XY
+        self.scores_ = np.max(pmi_XY, axis=1)
     #end def
 
-    def feature_select(self, X_featurized, Y_labels):
-        self.fit(X_featurized, Y_labels)
+    def feature_select(self, X_featurized, Y_labels, **kwargs):
+        self.fit(X_featurized, Y_labels, **kwargs)
 
-        return self.feature_scores_
+        return self.scores_
     #end def
+
+    def transform(self, X, **kwargs):
+        return X
 #end class
